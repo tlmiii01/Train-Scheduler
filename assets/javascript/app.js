ed58d2e45c1eacc8096f2e682f8494489554e314
Thomas Martin <tmartin.dev101010@gmail.com>
2018-09-14 09:22:30 -0600

    HTML wirefrom is done.

---
+++ assets/javascript/app.js
@@ -0,0 +1,12 @@
+var config = {
+    apiKey: "AIzaSyC9h2PUE_MqHDbAei_AROxKAEFDsayeOMI",
+    authDomain: "this-is-a-thomas-project.firebaseapp.com",
+    databaseURL: "https://this-is-a-thomas-project.firebaseio.com",
+    projectId: "this-is-a-thomas-project",
+    storageBucket: "this-is-a-thomas-project.appspot.com",
+    messagingSenderId: "811715918066"
+  };
+
+  firebase.initializeApp(config);
+
+  
